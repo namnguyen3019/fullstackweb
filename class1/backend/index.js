5501db27a821977a8b0e7d538ccfb5dd103daa1e
Nam Nguyen <namnguyn3019@gmail.com>
2021-01-27 12:22:14 -0600

    fix express

--- class1/backend/index.js
+++ class1/backend/index.js
@@ -1,18 +1,15 @@
-import express from 'express';
-import fs from 'fs';
-import cors from 'cors';
+import express from "express";
+import fs from "fs";
+import cors from "cors";
 
-const app = express()
-const port = 3000
-app.use(cors)
+const app = express();
+const port = 3000;
+app.use(cors);
 
 app.get("/", (req, res) => {
-    console.log("GET DATA FROM DATABASE")
-    fs.readFile("db.json", (error, content) => {
-        const items = JSON.parse(content)
-        res.json(items)
-    })
-})
+  res.send("Hello this is server");
+});
 
-
-app.listen(port, ()=> {console.log("Listen on port " + port)})
+app.listen(port, () => {
+  console.log("Listen on port " + port);
+});
