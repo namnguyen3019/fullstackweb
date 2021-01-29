9cce9d8c5ca904eff2b4136366517508e22d94db
Nam Nguyen <namnguyn3019@gmail.com>
2021-01-29 14:31:40 -0600

    fix cors + get data from db to show in frontend

--- class1/backend/index.js
+++ class1/backend/index.js
@@ -2,12 +2,20 @@ import express from "express";
 import fs from "fs";
 import cors from "cors";
 
+
 const app = express();
 const port = 3000;
-app.use(cors);
+app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello this is server");
+  fs.readFile('./db.json', (err, jsonString) => {
+    if (err){
+      console.log(err)
+    } else{
+      const data = JSON.parse(jsonString)
+      res.send(data)
+    }
+  })
 });
 
 app.listen(port, () => {
--- class1/frontend/main.js
+++ class1/frontend/main.js
@@ -2,78 +2,110 @@ var allTodoList = document.getElementById("allTodoList");
 
 // Add a new list
 window.onload = function () {
-  const server = "http://localhost:3000/";
-  fetch(server, {})
+  const server = "http://127.0.0.1:3000";
+  fetch(server)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
-    })
-    .then(console.log("Done fetching"));
+      for (todoListId in data) {
+        var listEle = document.createElement("div");
+        listEle.id = todoListId;
+        var titleEle = document.createElement("h2");
+        var titleText = document.createTextNode(todoListId);
+        titleEle.appendChild(titleText);
+        listEle.appendChild(titleEle);
+
+        // Adding the list title
+        allTodoList.appendChild(listEle);
+
+        // Adding the "Add to do " button
+        var addTodoBtn = document.createElement("button");
+        var BtnText = document.createTextNode("Add a new ToDo");
+        addTodoBtn.appendChild(BtnText);
+        addTodoBtn.onclick = function () {
+          addToDo(this);
+        };
+        listEle.appendChild(addTodoBtn);
+
+        var todoList = data[todoListId];
+        var ulElement = document.createElement("ul");
+        for (var i = 0; i < todoList.length; i++) {
+          var li = document.createElement("li");
+          var liText = document.createTextNode(todoList[i]);
+          li.appendChild(liText);
+          ulElement.appendChild(li);
+          listEle.appendChild(ulElement);
+        }
+      }
+    });
 };
 
 addToDo = (e) => {
-    parentElem = e.parentNode;
-  
-    var newLi = document.createElement("li");
-  
-    var newInput = document.createElement("input");
-  
-    //Submit button
-    var submitBtn = document.createElement("button");
-    var submitText = document.createTextNode("Submit");
-    submitBtn.id = "submitBtn";
-    submitBtn.appendChild(submitText);
-  
-    const cancelBtn = document.createElement("button");
-    cancelBtn.id = "cancelBtn";
-    const cancelText = document.createTextNode("Cancel");
-    cancelBtn.appendChild(cancelText);
-  
-    newLi.appendChild(newInput);
-    newLi.appendChild(submitBtn);
-    newLi.appendChild(cancelBtn);
-  
-    ulElement = parentElem.getElementsByTagName("ul");
-    if (Object.keys(ulElement).length == 0) {
-      console.log('Empty')
+  parentElem = e.parentNode;
+
+  var newLi = document.createElement("li");
+
+  var newInput = document.createElement("input");
+
+  //Submit button
+  var submitBtn = document.createElement("button");
+  var submitText = document.createTextNode("Submit");
+  submitBtn.id = "submitBtn";
+  submitBtn.appendChild(submitText);
+
+  const cancelBtn = document.createElement("button");
+  cancelBtn.id = "cancelBtn";
+  const cancelText = document.createTextNode("Cancel");
+  cancelBtn.appendChild(cancelText);
+
+  newLi.appendChild(newInput);
+  newLi.appendChild(submitBtn);
+  newLi.appendChild(cancelBtn);
+
+  ulElement = parentElem.getElementsByTagName("ul");
+  if (Object.keys(ulElement).length == 0) {
+    console.log("Empty");
+  } else {
+    ulElement[0].insertBefore(newLi, ulElement[0].firstChild);
+  }
+
+  // Submit new Todo
+  submitBtn.addEventListener("click", function (e) {
+    target = e.target
+    parent = target.parentNode.parentNode.parentNode;
+
+
+    var newTodoText = document
+      .getElementById(parent.id)
+      .getElementsByTagName("input")[0].value;
+
+    if (newTodoText.trim() !== "") {
+      newTodo = document.createTextNode(newTodoText);
+      // Append a new to do list
+      newLi.appendChild(newTodo);
+
+      // Remove input form
+      newLi.removeChild(newInput);
+      newLi.removeChild(submitBtn);
+      newLi.removeChild(cancelBtn);
     } else {
-      ulElement[0].insertBefore(newLi, ulElement[0].firstChild);
+      alert("It is empty string");
     }
-  
-    // Submit new Todo
-    submitBtn.addEventListener("click", function (e) {
-      target = e.target;
-      parent = target.parentNode.parentNode.parentNode;
-  
-      var newTodoText = document
-        .getElementById(parent.id)
-        .getElementsByTagName("input")[0].value;
-  
-      if (newTodoText.trim() !== "") {
-        newTodo = document.createTextNode(newTodoText);
-        // Append a new to do list
-        newLi.appendChild(newTodo);
-  
-        // Remove input form
-        newLi.removeChild(newInput);
-        newLi.removeChild(submitBtn);
-        newLi.removeChild(cancelBtn);
-      } else {
-        alert("It is empty string");
-      }
-    });
-  
-    // Cancel input a new todo list
-  
-    cancelBtn.addEventListener("click", function (e) {
-      console.log("Remove a new lis");
-      console.log(e.target)
-      ulElement.removeChild(e.target);
-    });
-  };
+  });
+
+  // Cancel input a new todo list
+
+  cancelBtn.addEventListener("click", function (e) {
+    const ul = e.target.parentNode.parentNode;
+    ul.removeChild(e.target.parentNode);
+  });
+};
 
 addANewList = (e) => {
   var newListTitle = document.getElementById("newList").value;
+  if (newListTitle.trim() === ''){
+    alert("empty title")
+    return
+  } 
   var newListId = newListTitle.trim().toLowerCase();
 
   var newListTitleContent = document.createTextNode(newListTitle);
@@ -90,7 +122,9 @@ addANewList = (e) => {
   // Add a new "add todo button" to the new list
   var newAddButton = document.createElement("button");
   var newAddButtonContent = document.createTextNode("Add a new Todo");
-  newAddButton.onclick = function(){addToDo(this)}
+  newAddButton.onclick = function () {
+    addToDo(this);
+  };
   newAddButton.appendChild(newAddButtonContent);
   newListDiv.appendChild(newAddButton);
 
@@ -105,5 +139,3 @@ addANewList = (e) => {
 cancelANewList = (e) => {
   document.getElementById("newList").value = "";
 };
-
-
